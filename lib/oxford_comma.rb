cf79c8a886be87f0633afc1b20cd85ca97cb79c3
NicMortelliti <npmortelliti@gmail.com>
2022-06-02 16:13:06 -0700

    Assignment complete

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  case array.length
+  when 0 then null
+  when 1 then array.join
+  when 2 then array.join(' and ')
+  else
+    new_string = [array[0..-2].join(', ')]
+    new_string << array.last
+    new_string.join(", and ")
+  end
 end
